2b03d913cda56f27ecb49c3063b42877f4632c70
Alexey Romarchuk <romarchuk@mail.ru>
2023-07-23 20:51:15 +0300

    Task № 1-3

---
+++ main.py
@@ -0,0 +1,70 @@
+import os
+from datetime import datetime
+# import logging
+
+def logger(old_function):
+   
+    def new_function(*args, **kwargs):
+        res = old_function (*args, **kwargs)
+        old_function_name = old_function.__name__
+        with open('main.log', 'a', encoding='utf8') as outfile:
+            outfile.write(f"Time: {datetime.now()}, F_name: {old_function_name}, Args: {args, kwargs}, Result: {res}, \n") 
+             
+        return res
+    return new_function
+   
+   
+    # def new_function(*args, **kwargs):
+    #     res = old_function (*args, **kwargs)
+    #     old_function_name = old_function.__name__
+    #     logging.basicConfig(
+    #         # level=logging.DEBUG,
+    #         filename = "main.log",
+    #         format = f"[%(asctime)s] - {old_function_name} - {args, kwargs} - {res}"
+    #         # datefmt='%H:%M:%S',
+    #         ) 
+    #     return res
+
+    # return new_function
+
+
+def test_1():
+
+    path = 'main.log'
+    if os.path.exists(path):
+        os.remove(path)
+
+    @logger
+    def hello_world():
+        return 'Hello World'
+
+    @logger
+    def summator(a, b=0):
+        return a + b
+
+    @logger
+    def div(a, b):
+        return a / b
+
+    assert 'Hello World' == hello_world(), "Функция возвращает 'Hello World'"
+    result = summator(2, 2)
+    assert isinstance(result, int), 'Должно вернуться целое число'
+    assert result == 4, '2 + 2 = 4'
+    result = div(6, 2)
+    assert result == 3, '6 / 2 = 3'
+    
+    assert os.path.exists(path), 'файл main.log должен существовать'
+
+    summator(4.3, b=2.2)
+    summator(a=0, b=0)
+
+    with open(path) as log_file:
+        log_file_content = log_file.read()
+
+    assert 'summator' in log_file_content, 'должно записаться имя функции'
+    for item in (4.3, 2.2, 6.5):
+        assert str(item) in log_file_content, f'{item} должен быть записан в файл'
+
+
+if __name__ == '__main__':
+    test_1()
